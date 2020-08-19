e2050652a559821c18b66f517a24fce68c5f9cd4
Alan Hamlett <alan.hamlett@gmail.com>
2020-08-19 10:42:05 -0700

    v1.0.3

--- flask_static_compress/__about__.py
+++ flask_static_compress/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'Flask-Static-Compress'
 __description__ = 'Auto-detects your static files for minification, combination, and versioning. Like Django-Compressor for Flask.'
 __url__ = 'https://github.com/alanhamlett/flask-static-compress'
-__version_info__ = ('1', '0', '2')
+__version_info__ = ('1', '0', '3')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Alan Hamlett'
 __author_email__ = 'alan.hamlett@gmail.com'
